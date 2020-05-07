69d58d1bc3913b8239c26c84ff1412d4e27921bb
evil-granny <den.ohorodnik@gmail.com>
2020-05-07 16:32:07 +0300

    Fixed problem with installation

---
+++ src/main/java/com/token/polls/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.token.polls.repository;
+
+import com.token.polls.model.Role;
+import com.token.polls.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
